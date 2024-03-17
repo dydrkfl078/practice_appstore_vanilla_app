9c907ba5844bc4cbd67e128a2d7bf4966b031b75
june <dydrkfl078@naver.com>
2024-03-18 02:38:00 +0900

    add-script/header_search_bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Shopping Bag
+
 const btnShopBagEl = document.querySelector("header .btn_shopping_bag");
 const shopBagEl = btnShopBagEl.querySelector(".shopping_bag");
 
@@ -25,4 +27,24 @@ function closeShopBag() {
 
 function openShopBag() {
   shopBagEl.classList.add("show");
+}
+
+// Header_SearchBar
+
+const headerEl = document.querySelector("header");
+const searchBtnEl = document.querySelector(".btn_search");
+const searchCloserEl = document.querySelector(".search-closer");
+const shadowEl = headerEl.querySelector(".shadow");
+
+searchBtnEl.addEventListener("click", showSearchBar);
+searchCloserEl.addEventListener("click", hideSearchBar);
+shadowEl.addEventListener("click", hideSearchBar);
+
+function showSearchBar() {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+}
+function hideSearchBar() {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
 }
